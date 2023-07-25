ad19697071f7ff5f0bc667da10986de248d585a1
kishan <kishanjethwa224@gmail.com>
2023-07-25 21:43:32 +0100

    Made the following changes: - Added a getProductName function that parses the link and looks for the search value, which should be the name of the product. - Changed what is presented to the user so that it is more user-friendly. For example, instead of just showing the average price, the user sees "Average price: 448.46". - The name of the product is also saved to the csv file so that the user can refer back to it easier as they'll now know what the product was.

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import numpy as np
 import csv
 from datetime import datetime
 
-LINK = input("Enter an eBay link to find the average price for the product! ")
+print("Welcome to the eBay Price Tracker!")
+print("Enter an eBay link to find the average price for the product! ")
+LINK = input("Input here: ")
 
 def get_prices_by_link(link):
     r = requests.get(link)
@@ -20,6 +22,12 @@ def get_prices_by_link(link):
         item_prices.append(price)
     return item_prices
 
+def getProductName(link):
+    r = requests.get(link)
+    page_parse = BeautifulSoup(r.text, 'html.parser')
+    input_tag = page_parse.find("input", {"class": "gh-tb ui-autocomplete-input"})
+    product_name = input_tag["value"]
+    return product_name
 
 def remove_outliers(prices, m=2):
     data = np.array(prices)
@@ -31,7 +39,9 @@ def get_avg(prices):
 
 
 def save_to_file(prices):
-    fields = [datetime.today().strftime("%B-%F-%Y"),np.around(get_avg(prices),2)]
+    fields = ["Date: "+datetime.today().strftime("%d-%m-%y"),
+              "Product name: "+getProductName(LINK),
+              "Average price: "+str(np.around(get_avg(prices), 2))]
     with open('prices.csv', 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
@@ -40,5 +50,6 @@ def save_to_file(prices):
 if __name__ == "__main__":
     prices = get_prices_by_link(LINK)
     without_outliers = remove_outliers(prices)
-    print(get_avg(without_outliers))
-    save_to_file(prices)
+    print("Product name: "+getProductName(LINK))
+    print("Average Price: "+str(np.around(get_avg(without_outliers), 2)))
+    save_to_file(without_outliers)
